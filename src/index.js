223966ac3006fee13742be864e1db7bfb9fc6968
thebunnyweb <hershd1@icloud.com>
2017-09-19 19:58:31 +0530

    refurbish

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, err=>{
+    if(!err){
+        console.log(`App running on port ${PORT}`)
+    }
+})
